a1dc2d2b6c9c0cd05d76e5ce36b02ba0fc95a224
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-08-01 21:06:23 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -eEo pipefail
+
+# Define Omarchy locations
+export ILV_PATH="$HOME/.local/share/ilv"
+export ILV_INSTALL="$ILV_PATH/install"
+export ILV_INSTALL_LOG_FILE="/var/log/ilv-install.log"
+export PATH="$ILV_PATH/bin:$PATH"
+
+# Install
+source "$ILV_INSTALL/helpers/all.sh"
+source "$ILV_INSTALL/preflight/all.sh"
+source "$ILV_INSTALL/packaging/all.sh"
+source "$ILV_INSTALL/config/all.sh"
+source "$ILV_INSTALL/login/all.sh"
+source "$ILV_INSTALL/post-install/all.sh"
