e778c060254512eb6eb92cf7e79fe47562d2bc2b
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-08-02 11:36:29 +0700

    Update pi.sh

--- install/config/pi.sh
+++ install/config/pi.sh
@@ -1,2 +1,2 @@
 mkdir -p ~/.pi/agent/extensions
-cp "$OMARCHY_PATH/default/pi/agent/extensions/omarchy-system-theme.ts" ~/.pi/agent/extensions/
+cp "$ILV_PATH/default/pi/agent/extensions/ILV-system-theme.ts" ~/.pi/agent/extensions/
