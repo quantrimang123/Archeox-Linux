fe03b15c7f6efd3eb110490ed4c766558f61dc4f
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-08-02 10:52:03 +0700

    Update omarchy-toggles.sh

--- install/config/omarchy-toggles.sh
+++ install/config/omarchy-toggles.sh
@@ -1,3 +1,3 @@
 # Toggles are used to turn certain features on/off in a persistent way
-mkdir -p ~/.local/state/omarchy/toggles/hypr
-cp $OMARCHY_PATH/default/hypr/toggles/flags.conf ~/.local/state/omarchy/toggles/hypr/
+mkdir -p ~/.local/state/ILV/toggles/hypr
+cp $ILV_PATH/default/hypr/toggles/flags.conf ~/.local/state/ILV/toggles/hypr/
