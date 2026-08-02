c09967f86dd4ca965fe930fbf9d43cbad63e0669
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-08-02 10:50:23 +0700

    Update branding.sh

--- install/config/branding.sh
+++ install/config/branding.sh
@@ -1,4 +1,4 @@
 # Allow the user to change the branding for fastfetch and screensaver
-mkdir -p ~/.config/omarchy/branding
-cp ~/.local/share/omarchy/icon.txt ~/.config/omarchy/branding/about.txt
-cp ~/.local/share/omarchy/logo.txt ~/.config/omarchy/branding/screensaver.txt
+mkdir -p ~/.config/ILV/branding
+cp ~/.local/share/ILV/icon.txt ~/.config/ILV/branding/about.txt
+cp ~/.local/share/ILV/logo.txt ~/.config/ILV/branding/screensaver.txt
