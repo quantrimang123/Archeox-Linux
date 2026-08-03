536a0b62d01747b96c675bc0f633ef7ffa9b5555
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-08-03 17:38:51 +0700

    Update ilv-toggles.sh

--- install/config/ilv-toggles.sh
+++ install/config/ilv-toggles.sh
@@ -1,3 +1,3 @@
 # Toggles are used to turn certain features on/off in a persistent way
 mkdir -p ~/.local/state/archeox/toggles/hypr
-cp $archeox_PATH/default/hypr/toggles/flags.conf ~/.local/state/archeox/toggles/hypr/
+cp $ARCHEOX_PATH/default/hypr/toggles/flags.conf ~/.local/state/archeox/toggles/hypr/
