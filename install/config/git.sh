7c796b54cc3cb5de36931fc88ca5dcc9320b3a9c
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-08-03 17:37:31 +0700

    Update git.sh

--- install/config/git.sh
+++ install/config/git.sh
@@ -1,8 +1,8 @@
 # Set identification from install inputs
-if [[ -n ${archeox_USER_NAME//[[:space:]]/} ]]; then
-  git config --global user.name "$archeox_USER_NAME"
+if [[ -n ${ARCHEOX_USER_NAME//[[:space:]]/} ]]; then
+  git config --global user.name "$ARCHEOX_USER_NAME"
 fi
 
-if [[ -n ${archeox_USER_EMAIL//[[:space:]]/} ]]; then
-  git config --global user.email "$archeox_USER_EMAIL"
+if [[ -n ${ARCHEOX_USER_EMAIL//[[:space:]]/} ]]; then
+  git config --global user.email "$ARCHEOX_USER_EMAIL"
 fi
