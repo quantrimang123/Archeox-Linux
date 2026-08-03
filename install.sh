2a7f193d826242073df8abbb0712f16cb549b5c3
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-08-03 17:33:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,16 @@
 # Exit immediately if a command exits with a non-zero status
 set -eEo pipefail
 
-# Define archeox locations
-export archeox_PATH="$HOME/.local/share/archeox"
-export archeox_INSTALL="$archeox_PATH/install"
-export archeox_INSTALL_LOG_FILE="/var/log/archeox-install.log"
-export PATH="$archeox_PATH/bin:$PATH"
+# Define Archeox locations
+export ARCHEOX_PATH="$HOME/.local/share/archeox"
+export ARCHEOX_INSTALL="$ARCHEOX_PATH/install"
+export ARCHEOX_INSTALL_LOG_FILE="/var/log/archeox-install.log"
+export PATH="$ARCHEOX_PATH/bin:$PATH"
 
 # Install
-source "$archeox_INSTALL/helpers/all.sh"
-source "$archeox_INSTALL/preflight/all.sh"
-source "$archeox_INSTALL/packaging/all.sh"
-source "$archeox_INSTALL/config/all.sh"
-source "$archeox_INSTALL/login/all.sh"
-source "$archeox_INSTALL/post-install/all.sh"
+source "$ARCHEOX_INSTALL/helpers/all.sh"
+source "$ARCHEOX_INSTALL/preflight/all.sh"
+source "$ARCHEOX_INSTALL/packaging/all.sh"
+source "$ARCHEOX_INSTALL/config/all.sh"
+source "$ARCHEOX_INSTALL/login/all.sh"
+source "$ARCHEOX_INSTALL/post-install/all.sh"
