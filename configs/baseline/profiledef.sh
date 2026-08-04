2d751089e83b5aa3bea724b299f6a8e70eb3e6e9
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-08-04 07:38:06 +0700

    Update profiledef.sh

--- configs/baseline/profiledef.sh
+++ configs/baseline/profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="Archeox Linux-baseline"
+iso_name="Archeox Linux baseline"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Archeox Linux <hd070812@gmail.com>"
 iso_application="Archeox Linux baseline"
